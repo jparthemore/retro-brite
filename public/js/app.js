4a13500976eaebf432223c2ea18a722da08a6efb
Jan Parthemore <janparthemore@gmail.com>
2017-06-15 20:32:48 -0400

    refactor the makeGridd method. Also implemented the rendder method to draw the grid on the page that required the cell to produce a dom element

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,10 @@
 const App = {
   rootElement: '#app',
   selectedColor: 'white',
-  numRows: 4,
-  numCols: 4,
-  cellWidth: 100,
-  cellHeight: 100,
+  numRows: 10,
+  numCols: 10,
+  cellWidth: 25,
+  cellHeight: 25,
   grid: [],
   start: function(){
     this.cacheDOM();//dom caaches any static things need to keep track of
@@ -15,22 +15,31 @@ const App = {
   },
   makeGrid: function(){
     this.grid = new Array(this.numRows);
-    for(let i = 0; i<this.grid.length; i++){
-      this.grid[i]= new Array(this.numCols);
-    }
-    for(let i = 0; i< this.grid.length; i++){
-      for (let j=0; j < this.grid[i].length; j++){
-        //tempGrid[i][j] = i + j;
-        this.grid[i][j] = new Cell(this.cellWidth, this.cellHeight, this.selectedColor);
+
+    for(let rowIndex = 0; rowIndex< this.numRows; rowIndex++){
+      this.grid[rowIndex]= new Array(this.numCols);
+      for (let colIndex=0; colIndex < this.numCols; colIndex++){
+        this.grid[rowIndex][colIndex] = new Cell(this.cellWidth, this.cellHeight, this.selectedColor);
       }
     }
-    console.log(this.grid);
 
   },
-  cacheDOM: function(){},
+  cacheDOM: function(){
+    this.root = document.querySelector(this.rootElement);
+  },
 
   bindEvents: function(){},
-  render: function(){}
+  render: function(){
+    this.grid.forEach(row=>{
+      const rowContainer = document.createElement('div');
+      rowContainer.style.height = `${this.cellHeight}px`;
+      row.forEach(cell=>{
+        const element = cell.toHtml();
+        rowContainer.appendChild(element);
+      });
+      this.root.appendChild(rowContainer);
+    });
+  }
 
 };
 
